f9ef326afb63ec1f1b7124e83c2b2eabacad7aa9
RomanSaykevich <saykevichroman628@gmail.com>
2022-06-22 23:41:20 +0300

    o

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 
-import Simpsons from "./components/simpsons/Simpsons";
-import RickandMortys from "./components/rickandmortys/RickandMortys";
-
 const App = () => {
-
     return (
-
         <div>
-            <Simpsons/>
-            <RickandMortys/>
+            
         </div>
-
     );
-
 };
 
 export default App;
