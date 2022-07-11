cd73448d46a907759ca75258ed17e1e16e0e7b88
RomanSaykevich <saykevichroman628@gmail.com>
2022-07-11 22:24:41 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {NavLink, Route, Router} from "react-router-dom";
 
 const App = () => {
     return (
         <div>
-            
+            <div>
+    <ul>
+        <li><NavLink to={''}></NavLink></li>
+        <li></li>
+    </ul>
+            </div>
+            <div>
+
+            </div>
+
+
         </div>
     );
 };
